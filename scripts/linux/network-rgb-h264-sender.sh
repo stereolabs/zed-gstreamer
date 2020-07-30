8b1de376311df535d669db808683b828e7776ab1
Walter Lucetti <walter.lucetti@stereolabs.com>
2020-07-30 17:43:47 +0200

    Improved local network sender script using H264 under Linux

--- scripts/linux/network-rgb-h264-sender.sh
+++ scripts/linux/network-rgb-h264-sender.sh
@@ -6,7 +6,7 @@
 
 #gst-launch-1.0 zedsrc ! queue ! autovideoconvert ! queue ! fpsdisplaysink
 
-gst-launch-1.0 zedsrc resolution=2 ! autovideoconvert ! timeoverlay ! tee name=split ! \
+gst-launch-1.0 zedsrc resolution=2 ! queue ! autovideoconvert ! timeoverlay ! tee name=split ! \
  queue ! fpsdisplaysink \
- split. ! queue ! x264enc threads=4 tune=zerolatency ! h264parse ! \
- rtph264pay config-interval=1 pt=96 ! udpsink host=127.0.0.1 port=5000 sync=false async=false
+ split. ! queue ! x264enc tune=zerolatency ! h264parse ! \
+ rtph264pay config-interval=1 pt=96 ! queue ! udpsink host=192.168.0.52 port=5000 sync=false async=false
