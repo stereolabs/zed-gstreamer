ec8c6b94a19459c2d35faac717621133af13a4e3
Walter Lucetti <walter.lucetti@stereolabs.com>
2020-07-31 18:02:38 +0200

    Add start code for RTSP server

--- scripts/linux/network-rgb-h264-sender.sh
+++ scripts/linux/network-rgb-h264-sender.sh
@@ -6,7 +6,7 @@
 
 #gst-launch-1.0 zedsrc ! queue ! autovideoconvert ! queue ! fpsdisplaysink
 
-gst-launch-1.0 zedsrc resolution=2 ! queue ! autovideoconvert ! timeoverlay ! tee name=split ! \
- queue ! fpsdisplaysink \
- split. ! queue ! x264enc tune=zerolatency ! h264parse ! \
- rtph264pay config-interval=1 pt=96 ! queue ! udpsink host=192.168.0.52 port=5000 sync=false async=false
+gst-launch-1.0 zedsrc resolution=1 framerate=30 ! queue ! timeoverlay ! identity drop-allocation=true ! tee name=split ! \
+ queue ! videoconvert ! queue ! fpsdisplaysink \
+ split. ! queue ! videoconvert ! x264enc byte-stream=true tune=zerolatency speed-preset=ultrafast bitrate=3000 qp-min=0 qp-min=0 qp-step=0 ! h264parse ! \
+ rtph264pay config-interval=-1 pt=96 ! queue ! udpsink clients=192.168.0.50:5000,192.168.0.52:5000 max-bitrate=3000000 sync=false async=false
