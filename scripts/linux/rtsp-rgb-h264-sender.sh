0c8891a97d8142f025290f2dbc71cb8c99099a7f
Walter Lucetti <walter.lucetti@stereolabs.com>
2020-08-03 19:27:44 +0200

    zed-rtsp-server working  * script to send RGB image to RTSP using H264

--- scripts/linux/rtsp-rgb-h264-sender.sh
+++ scripts/linux/rtsp-rgb-h264-sender.sh
@@ -5,6 +5,12 @@
 # install RTSP libraries to compile RTSP server:
 # `sudo apt install libgstrtspserver-1.0-0 libgstrtspserver-1.0-dev`
 
-./gst-rtsp-launch "( zedsrc resolution=2 ! videoconvert ! video/x-raw, format=(string)I420 ! x264enc byte-stream=false tune=zerolatency speed-preset=ultrafast bitrate=3000 qp-min=0 qp-min=0 qp-step=0 ! rtph264pay config-interval=-1 pt=96 name=pay0 )"
+HOST_IPS=(`hostname -I`)
+export SERVER_IP=${HOST_IPS[0]}
 
 
+gst-zed-rtsp-launch -a ${SERVER_IP} \
+ zedsrc resolution=2 framerate=30 stream-type=0 ! \
+ videoconvert ! video/x-raw, format=I420 ! \
+ x264enc tune=zerolatency bitrate=5000 speed-preset=ultrafast key-int-max=60 qp-min=8 qp-max=51 qp-step=1 ! \
+ rtph264pay config-interval=-1 mtu=1500 pt=96 name=pay0
