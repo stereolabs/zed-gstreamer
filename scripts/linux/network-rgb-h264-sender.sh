cb601fb9d4a5a1c42a66f5780535ee5816f417fd
Walter Lucetti <walter.lucetti@stereolabs.com>
2020-08-05 17:38:04 +0200

    Network scripts

--- scripts/linux/network-rgb-h264-sender.sh
+++ scripts/linux/network-rgb-h264-sender.sh
@@ -4,9 +4,7 @@
 
 # install x264enc: `sudo apt-get install gstreamer1.0-libav x264 gstreamer1.0-plugins-ugly`
 
-#gst-launch-1.0 zedsrc ! queue ! autovideoconvert ! queue ! fpsdisplaysink
-
 gst-launch-1.0 zedsrc resolution=1 framerate=30 ! queue ! timeoverlay ! identity drop-allocation=true ! tee name=split ! \
  queue ! videoconvert ! queue ! fpsdisplaysink \
  split. ! queue ! videoconvert ! x264enc byte-stream=true tune=zerolatency speed-preset=ultrafast bitrate=3000 qp-min=0 qp-min=0 qp-step=0 ! h264parse ! \
- rtph264pay config-interval=-1 pt=96 ! queue ! udpsink clients=192.168.0.50:5000,192.168.0.52:5000 max-bitrate=3000000 sync=false async=false
+ rtph264pay config-interval=-1 pt=96 ! queue ! udpsink clients=192.168.0.50:5000,192.168.0.51:5000,192.168.0.52:5000 max-bitrate=3000000 sync=false async=false
