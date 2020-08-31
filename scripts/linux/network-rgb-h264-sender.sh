2d9c03803ef2186d515fe80f06e035a4eebba167
Walter Lucetti <walter.lucetti@stereolabs.com>
2020-08-31 12:02:51 +0200

    Fixes

--- scripts/linux/network-rgb-h264-sender.sh
+++ scripts/linux/network-rgb-h264-sender.sh
@@ -6,7 +6,7 @@
 
 gst-launch-1.0 zedsrc camera-resolution=2 camera-fps=30 ! timeoverlay ! tee name=split has-chain=true ! \
  queue ! autovideoconvert ! fpsdisplaysink \
- split. ! queue max-size-time=0 max-size-bytes=0 max-size-buffers=0 ! autovideoconvert ! \
+ split. !  queue max-size-time=0 max-size-bytes=0 max-size-buffers=0 ! autovideoconvert ! \
  x264enc byte-stream=true tune=zerolatency speed-preset=ultrafast bitrate=3000 ! \
  h264parse ! rtph264pay config-interval=-1 pt=96 ! queue ! \
  udpsink clients=127.0.0.1:5000 max-bitrate=3000000 sync=false async=false
