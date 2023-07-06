0726f0658b46e1613c48800d8f5b510d1b6ebbff
Walter Lucetti <walter.lucetti@stereolabs.com>
2023-07-06 15:35:19 +0200

    Improve jetson scripts

--- scripts/jetson/local-rgb-rescale-od-overlay.sh
+++ scripts/jetson/local-rgb-rescale-od-overlay.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 
-# Example pipeline to resize 2K RGB and depth stream to VGA resolution for display purpose remuxing the Object 
+# Example pipeline to resize HD1080 RGB and depth stream to VGA resolution for display purpose remuxing the Object 
 # Detection metadata as input for `zedodoverlay`
 
 # 1) Define a `zeddatamux` object named `mux` to be used to recompose the RGB+metadata stream to be used to render 
 #    the result of Object Detection using `zedoddisplaysink`. The `videoscale` filter indeed removes ZED metadata
 #    from the RGB stream and the `zeddatamux` adds them back
-# 2) Start `zedsrc` to acquire RGB and depth at 2K resolution stream enabling Skeleton Tracking (FAST).
+# 2) Start `zedsrc` to acquire RGB and depth at HD1080 resolution stream enabling Object Tracking
 # 3) Define `zeddemux` object named `demux` to demux the `zedsrc` composite stream creating three single streams: RGB, Depth and Data.
 # 4) Rescale the Depth stream to VGA resolution and render it using `fpsdisplaysink`.
 # 5) Set the data stream from `demux` as input on the data sink of `mux`.
@@ -16,7 +16,7 @@
 
 gst-launch-1.0 \
 zeddatamux name=mux \
-zedsrc stream-type=4 camera-resolution=0 camera-fps=30 od-detection-model=1 od-enabled=true ! \
+zedsrc stream-type=4 camera-resolution=1 camera-fps=30 od-detection-model=0 od-enabled=true ! \
 zeddemux stream-data=true is-depth=true name=demux \
 demux.src_aux ! queue ! autovideoconvert ! videoscale ! video/x-raw,width=672,height=376 ! queue ! fpsdisplaysink \
 demux.src_data ! mux.sink_data \
--- scripts/jetson/simple-fps_rendering.sh
+++ scripts/jetson/simple-fps_rendering.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # Example pipeline to acquire a stream and render it dispaying the current FPS using default values for each parameter
 
