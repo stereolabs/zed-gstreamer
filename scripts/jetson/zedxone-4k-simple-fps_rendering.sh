a46cc4d0cff6dfa0e0e19565e58d4c608a84aa3b
Walter Lucetti <walter.lucetti@stereolabs.com>
2024-04-10 19:45:08 +0200

    Fix 4k script

--- scripts/jetson/zedxone-4k-simple-fps_rendering.sh
+++ scripts/jetson/zedxone-4k-simple-fps_rendering.sh
@@ -2,4 +2,4 @@
 
 # Example pipeline to acquire a ZED X One 4K stream at 15 FPS and render it displaying the current FPS using default values for each parameter
 
-gst-launch-1.0 zedxonesrc camera-resolution=2 camera-fps=15 ! queue ! autovideoconvert ! queue ! fpsdisplaysink
+gst-launch-1.0 zedxonesrc camera-resolution=3 camera-fps=15 ! queue ! autovideoconvert ! queue ! fpsdisplaysink
